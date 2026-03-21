5c3b4f7878eeedf9f42b309a4153daf75febd19a
Sami Sieranoja <sami.sieranoja@gmail.com>
2026-03-21 14:57:55 +0200

    travel decision example

--- src/nn_travel_decision.py
+++ src/nn_travel_decision.py
@@ -6,23 +6,33 @@ import torch.nn.functional as F
 class TravelDecision(nn.Module):
 	def __init__(self):
 		super(TravelDecision, self).__init__()
-		self.fc1 = nn.Linear(2, 2)
-		self.fc2 = nn.Linear(2, 1)
-		self.fc3 = nn.Linear(1, 2)
+		
+		# Three layers
+		self.fc1 = nn.Linear(2, 2) # two inputs, two neurons
+		self.fc2 = nn.Linear(2, 1) # two inputs, one neuron
+		self.fc3 = nn.Linear(1, 2) # one input, two neurons
 		
 	def forward(self, x):
 		x = F.relu(self.fc1(x))
-		print(x)
+		print(f"Layer 1 output: {x.detach().cpu().numpy()}")
+		
+		
+		# x = F.tanh(self.fc2(x))
+		# https://docs.pytorch.org/docs/stable/generated/torch.nn.Tanh.html
+		
 		x = self.fc2(x)
-		print(x)
+		print(f"Layer 2 output: {x.detach().cpu().numpy()}")
+		
 		x = F.relu(self.fc3(x))
 		return x
 		
 model = TravelDecision()
 
 # Disable automatic differentiation
-# Set the network weights
 with torch.no_grad():
+
+  # Set the network weights
+
 	# first row [-8,-1] corresponds to h3 weights
 	# second row corresponds to h4 weights
 	model.fc1.weight.copy_(torch.tensor([[-8,-1], [-1,-0.01]]))       
@@ -34,15 +44,17 @@ with torch.no_grad():
 	model.fc3.weight.copy_(torch.tensor([[1],[-1]]))       
 	model.fc3.bias.copy_(torch.tensor([0]))
 
-
 print("Good Weather (0.2):")
-print(model(torch.tensor([0.2,4.0])))
+x = torch.tensor([0.2,4.0])
+print(f"Final output: {model(x).detach().cpu().numpy()}\n")
 
 print("Bad Weather (0.8):")
-print(model(torch.tensor([0.8,4.0])))
+x = torch.tensor([0.8,4.0])
+print(f"Final output: {model(x).detach().cpu().numpy()}\n")
 
 print("Long distance (440):")
-print(model(torch.tensor([0.8,440.0])))
+x = torch.tensor([0.8,440.0])
+print(f"Final output: {model(x).detach().cpu().numpy()}\n")
 
 
 
